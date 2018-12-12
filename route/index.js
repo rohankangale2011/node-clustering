36c18841ef70020708e51f4121e9188d5208228a
Rohan Kangale <Rohan.Kangale@ezest.local>
2018-12-12 15:37:49 +0530

    webhook api

--- route/index.js
+++ route/index.js
@@ -1,12 +1,16 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-router.get('/', function(req, res) {
-    res.send('Default API');
+router.get("/", function(req, res) {
+  res.send("Default API");
 });
 
-router.get('/get', function(req, res) {
-    res.send('Calling get API');
+router.get("/get", function(req, res) {
+  res.send("Calling get API");
+});
+
+router.post("/webhook", function(req, res) {
+  res.send("Calling get API");
 });
 
 module.exports = router;
